57035277600a1f7abba78e837883bba2dd397634
lyorafelicya <13520073@std.stei.itb.ac.id>
2022-10-23 14:57:06 +0700

    add homepage

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,25 +1,96 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
-  </head>
-  <body>
-    <!--[if lt IE 7]>
-      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-    <![endif]-->
-    <a href="/?login">Login</a>
-    <p>
-      Ini home
-    </p>
-    <script src="" async defer></script>
-  </body>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title> Binotify </title>
+        <link rel="stylesheet" href="../../../public/css/home.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+
+    <body>
+        <section class="header">
+            <a href="/?home" class="logo"> Binotify </a>
+
+            <!-- <input type="text" class="searchTerm" placeholder="What do you want to listen to?"> -->
+            <!-- <div class="search">
+                <input type="text" class="searchTerm" placeholder="What do you want to listen to?"> -->
+                <!-- <button type="submit"><i class="fa fa-search"></i></button> -->
+            <!-- </div> -->
+    
+            <nav class="navbar">
+                <a href="album.html"> Album </a>
+                <a href="search.html"> Search </a>
+                <a href="/?login"> Login </a>
+                <a href="logout.html"> Sign Up </a>
+            </nav>
+        </section>
+
+        <!-- <h1 class="user"> Hello, <span class="username">Tiara!</span></h1> -->
+        <div class="song">
+            <h1> Discover </h1>
+            <h2> Filter </h2> <h2> Sort By </h2>
+            <div class="song-list">
+                <div class="song-card">
+                    <span> 01 </span>
+                    <img src="../../../public/img/new jeans.jpeg" alt="">
+                    <h5>
+                        Attention
+                        <div class="singer">New Jeans</div>
+                        <span class="genre">K-pop</span>
+                        <span class="genre">R&B</span>
+                    </h5>
+                    <div class="year">2022</div>
+                </div>
+
+                <div class="song-card">
+                    <span> 02 </span>
+                    <img src="../../../public/img/L.jpeg" alt="">
+                    <h5>
+                        L
+                        <div class="singer">Hal</div>
+                        <span class="genre">Indie</span>
+                    </h5>
+                    <div class="year">2022</div>
+                </div>
+
+                <div class="song-card">
+                    <span> 03 </span>
+                    <img src="../../../public/img/sang dewi.jpeg" alt="">
+                    <h5>
+                        Sang Dewi
+                        <div class="singer">Lyodra, Andi Rianto</div>
+                        <span class="genre">Pop</span>
+                    </h5>
+                    <div class="year">2022</div>
+                </div>
+
+                <div class="song-card">
+                    <span> 04 </span>
+                    <img src="../../../public/img/2soon.jpeg" alt="">
+                    <h5>
+                        2 Soon
+                        <div class="singer">Keshi</div>
+                        <span class="genre">Soul</span>
+                        <span class="genre">R&B</span>
+                    </h5>
+                    <div class="year">2022</div>
+                </div>
+
+                <div class="song-card">
+                    <span> 05 </span>
+                    <img src="../../../public/img/child.jpeg" alt="">
+                    <h5>
+                        Child
+                        <div class="singer">Mark</div>
+                        <span class="genre">Hip Hop</span>
+                    </h5>
+                    <div class="year">2022</div>
+                </div>
+            </div>
+        </div>
+    
+        <script src="homepage.js"></script>
+
+    </body>
 </html>
