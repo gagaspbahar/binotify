f026f81b1fbe8d95fc557f6be777b1757dd80c4f
lyorafelicya <13520073@std.stei.itb.ac.id>
2022-10-24 20:45:20 +0700

    styling homepage mirip spotify

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -6,91 +6,219 @@
         <title> Binotify </title>
         <link rel="stylesheet" href="../../../public/css/home.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+            integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
     </head>
 
     <body>
-        <section class="header">
-            <a href="/?home" class="logo"> Binotify </a>
+        <div class="main-contaner">
+            <div class="homepage">
+                <div class="side-navbar-container">
+                   <img src="../../../public/img/logo.png" alt="" class="logo">
+                   <nav class="navbar">
+                        <ul>
+                            <li><i class="fas fa-home"></i><a href="/?home"> Home </a></li>
+                            <li><i class="fas fa-search"></i><a href="search.html"> Search </a></li>
+                            <li><i class="fas fa-list"></i><a href="album.html"> Album </a></li>
+                            <hr class="rounded">
+                            <li><a href="/?login"> Login </a></li>
+                            <li><a href="/?register"> Sign Up </a></li>
+                        </ul>
+                    </nav>
+                </div>
 
-            <!-- <input type="text" class="searchTerm" placeholder="What do you want to listen to?"> -->
-            <!-- <div class="search">
-                <input type="text" class="searchTerm" placeholder="What do you want to listen to?"> -->
-                <!-- <button type="submit"><i class="fa fa-search"></i></button> -->
-            <!-- </div> -->
+                <div class="homepage-container">
+                    <nav class="profile-navbar">
+                         <a href="album.html" class="user"> Hello, User </a>
+                    </nav>
+
+                    <div class="song-container">
+                        <h1> Good Afternoon </h1>
+
+                        <h2 class="top-10-newest">Top 10</h2>
+
+                        <div class="songlist-container">
+                            <ul class="songlist">
+                               <li class='songlist-row'>
+                                    <div class='song-count'>
+                                        <img class='play' src='../../../public/img/play-white.png'>
+                                        <span class='song-number'>1.</span>
+                                    </div>
+
+                                    <div class="song-image">
+                                        <img class='songimage' src='../../../public/img/new jeans.jpeg'>
+                                    </div>
+
+                                    <div class='song-info'>
+                                        <span class='song-title'>Attention</span>
+                                        <span class='singer'>New Jeans</span>
+                                    </div>
+
+                                    <div class='song-releasedate'>
+                                        <span class='release-date'>July 22, 2022</span>
+                                    </div>
+
+                                    <div class='song-genre'>
+                                        <span class='genre'>K-pop</span>
+                                    </div>
+
+                                    <div class='trackOptions'>
+                                        <img class='optionButton' src='../../../public/img/more.png'>
+                                    </div>
+                                </li>
+
+
+                                <li class='songlist-row'>
+                                    <div class='song-count'>
+                                        <img class='play' src='../../../public/img/play-white.png'>
+                                        <span class='song-number'>2.</span>
+                                    </div>
+
+                                    <div class="song-image">
+                                        <img class='songimage' src='../../../public/img/L.jpeg'>
+                                    </div>
+
+                                    <div class='song-info'>
+                                        <span class='song-title'>L</span>
+                                        <span class='singer'>Hal</span>
+                                    </div>
+
+                                    <div class='song-releasedate'>
+                                        <span class='release-date'>July 22, 2022</span>
+                                    </div>
+
+                                    <div class='song-genre'>
+                                        <span class='genre'>Indie</span>
+                                    </div>
+
+                                    <div class='trackOptions'>
+                                        <img class='optionButton' src='../../../public/img/more.png' onclick='showOptionsMenu(this)'>
+                                    </div>
+                                </li>
+
+
+                                <li class='songlist-row'>
+                                    <div class='song-count'>
+                                        <img class='play' src='../../../public/img/play-white.png'>
+                                        <span class='song-number'>3.</span>
+                                    </div>
+
+                                    <div class="song-image">
+                                        <img class='songimage' src='../../../public/img/sang dewi.jpeg'>
+                                    </div>
+
+                                    <div class='song-info'>
+                                        <span class='song-title'>Sang Dewi</span>
+                                        <span class='singer'>Lyodra</span>
+                                    </div>
+
+                                    <div class='song-releasedate'>
+                                        <span class='release-date'>July 22, 2022</span>
+                                    </div>
+
+                                    <div class='song-genre'>
+                                        <span class='genre'>Indo</span>
+                                    </div>
+
+                                    <div class='trackOptions'>
+                                        <img class='optionButton' src='../../../public/img/more.png'>
+                                    </div>
+                                </li>
+
+
+                                <li class='songlist-row'>
+                                    <div class='song-count'>
+                                        <img class='play' src='../../../public/img/play-white.png'>
+                                        <span class='song-number'>4.</span>
+                                    </div>
+
+                                    <div class="song-image">
+                                        <img class='songimage' src='../../../public/img/child.jpeg'>
+                                    </div>
+
+                                    <div class='song-info'>
+                                        <span class='song-title'>Child</span>
+                                        <span class='singer'>Mark</span>
+                                    </div>
+
+                                    <div class='song-releasedate'>
+                                        <span class='release-date'>July 22, 2022</span>
+                                    </div>
+
+                                    <div class='song-genre'>
+                                        <span class='genre'>K-pop</span>
+                                    </div>
+
+                                    <div class='trackOptions'>
+                                        <!-- <input type='hidden' class='songId' value='" . $albumSong->getId() . "'> -->
+                                        <img class='optionButton' src='../../../public/img/more.png'>
+                                    </div>
+                                </li>
+
+
+                                <li class='songlist-row'>
+                                    <div class='song-count'>
+                                        <img class='play' src='../../../public/img/play-white.png'>
+                                        <span class='song-number'>5.</span>
+                                    </div>
+
+                                    <div class="song-image">
+                                        <img class='songimage' src='../../../public/img/2soon.jpeg'>
+                                    </div>
+
+                                    <div class='song-info'>
+                                        <span class='song-title'>2soon</span>
+                                        <span class='singer'>Keshi</span>
+                                    </div>
+
+                                    <div class='song-releasedate'>
+                                        <span class='release-date'>July 22, 2022</span>
+                                    </div>
+
+                                    <div class='song-genre'>
+                                        <span class='genre'>Indie</span>
+                                    </div>
+
+                                    <div class='trackOptions'>
+                                        <input type='hidden' class='songId' value='" . $albumSong->getId() . "'>
+                                        <img class='optionButton' src='../../../public/img/more.png'>
+                                    </div>
+                                </li>         
+                            </ul>
+                        </div>
+
+                    </div>
+
+                </div>
     
-            <nav class="navbar">
-                <a href="album.html"> Album </a>
-                <a href="search.html"> Search </a>
-                <a href="/?login"> Login </a>
-                <a href="/?register"> Sign Up </a>
-            </nav>
-        </section>
-
-        <!-- <h1 class="user"> Hello, <span class="username">Tiara!</span></h1> -->
-        <div class="song">
-            <h1> Discover </h1>
-            <h2> Filter </h2> <h2> Sort By </h2>
-            <div class="song-list">
-                <div class="song-card">
-                    <span> 01 </span>
-                    <img src="../../../public/img/new jeans.jpeg" alt="">
-                    <h5>
-                        Attention
-                        <div class="singer">New Jeans</div>
-                        <span class="genre">K-pop</span>
-                        <span class="genre">R&B</span>
-                    </h5>
-                    <div class="year">2022</div>
-                </div>
-
-                <div class="song-card">
-                    <span> 02 </span>
-                    <img src="../../../public/img/L.jpeg" alt="">
-                    <h5>
-                        L
-                        <div class="singer">Hal</div>
-                        <span class="genre">Indie</span>
-                    </h5>
-                    <div class="year">2022</div>
-                </div>
-
-                <div class="song-card">
-                    <span> 03 </span>
-                    <img src="../../../public/img/sang dewi.jpeg" alt="">
-                    <h5>
-                        Sang Dewi
-                        <div class="singer">Lyodra, Andi Rianto</div>
-                        <span class="genre">Pop</span>
-                    </h5>
-                    <div class="year">2022</div>
-                </div>
-
-                <div class="song-card">
-                    <span> 04 </span>
-                    <img src="../../../public/img/2soon.jpeg" alt="">
-                    <h5>
-                        2 Soon
-                        <div class="singer">Keshi</div>
-                        <span class="genre">Soul</span>
-                        <span class="genre">R&B</span>
-                    </h5>
-                    <div class="year">2022</div>
-                </div>
-
-                <div class="song-card">
-                    <span> 05 </span>
-                    <img src="../../../public/img/child.jpeg" alt="">
-                    <h5>
-                        Child
-                        <div class="singer">Mark</div>
-                        <span class="genre">Hip Hop</span>
-                    </h5>
-                    <div class="year">2022</div>
-                </div>
             </div>
-        </div>
-    
-        <script src="homepage.js"></script>
 
+            
+
+                    <!-- <div class="playlist-card">
+                        <i class="fas fa-home"></i>
+                        <h3 class="playlist-main-content">Home</h3>
+    
+                    </div>
+                    <div class="playlist-card">
+                       
+                        <i class="fas fa-search"></i>
+                        <h3 class="playlist-main-content">Search</h3>
+    
+                    </div>
+                    <div class="playlist-card">
+                        <i class="fas fa-list"></i>
+                        <h3 class="playlist-main-content">Play List</h3>
+                    </div> -->
+            
+
+            <!-- <div class="music-player"> 
+            </div> -->
+        </div>
     </body>
 </html>
