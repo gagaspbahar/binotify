3f6cef00900a5ee69d5013387fb78362a4eedd4d
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2022-10-27 01:22:17 +0700

    add search MODE GAUL

--- app/views/search/index.php
+++ app/views/search/index.php
@@ -4,43 +4,50 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="../../../public/js/search.js"></script>
-    <link rel="stylesheet" href="">
-  </head>
-  <body>
-    <!--[if lt IE 7]>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="../../../public/js/search.js"></script>
+  <link rel="stylesheet" href="">
+</head>
+
+<body>
+  <script>
+    searchSong("<?php echo $data['path'] ?>");
+  </script>
+  <!--[if lt IE 7]>
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
 
-    <input type="text" id="search-input"> </input>
+  <input type="text" id="search-input"> </input>
 
-    <select name="sort" id="sort">
-      <option value="">none</option>
-      <option value="_judul">judul asc</option>
-      <option value="-judul">judul desc</option>
-      <option value="_tanggal_terbit">tanggal terbit asc</option>
-      <option value="-tanggal_terbit">tanggal terbit desc</option>
-    </select>
-    <select name="filter" id="filter">
-      <option value="">none</option>
-      <option value="R&B">R&B</option>
-      <option value="Rock">Rock</option>
-      <option value="Pop">Pop</option>
-    </select>
+  <select name="sort" id="sort">
+    <option value="">none</option>
+    <option value="_judul">judul asc</option>
+    <option value="-judul">judul desc</option>
+    <option value="_tanggal_terbit">tanggal terbit asc</option>
+    <option value="-tanggal_terbit">tanggal terbit desc</option>
+  </select>
+  <select name="filter" id="filter">
+    <option value="">none</option>
+    <option value="R&B">R&B</option>
+    <option value="Rock">Rock</option>
+    <option value="Pop">Pop</option>
+  </select>
 
-    <input type="text" id="page"></input>
-    
-    <button type="button" onclick=searchSong()>Search</button>
-    <div id="song-list"></div>
-  </body>
+  <input type="text" id="page"></input>
 
-  
+  <button type="button" onclick=searchSong()>Search</button>
+  <div id="song-list"></div>
+  <button type="button" onclick=prevPage()>Prev</button>
+  <button type="button" onclick=nextPage()>Next</button>
+</body>
+
+
 </html>
 
 <!-- sort by abjad, tahun terbit -->
--- public/js/search.js
+++ public/js/search.js
@@ -1,34 +1,72 @@
-const searchSong = () => {
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (decodeURIComponent(pair[0]) == variable) {
+          return decodeURIComponent(pair[1]);
+      }
+  }
+  console.log('Query variable %s not found', variable);
+}
+
+function updateQueryStringParameter(uri, key, value) {
+  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  if (uri.match(re)) {
+    return uri.replace(re, '$1' + key + "=" + value + '$2');
+  }
+  else {
+    return uri + separator + key + "=" + value;
+  }
+}
+
+const searchSong = (data = "") => {
   console.log("terpanggil");
-  const title = document.getElementById("search-input").value;
-  const sort = document.getElementById("sort").value;
-  const filter = document.getElementById("filter").value;
-  console.log(filter);
-  const page = document.getElementById("page").value;
   let base_url = "../../api/music/search.php?";
-  if (title.length > 0) {
-    base_url += "judul=" + title;
+  let add_url = "";
+  
+  try{
+    const title = document.getElementById("search-input").value;
+    const sort = document.getElementById("sort").value;
+    const filter = document.getElementById("filter").value;
+    const page = document.getElementById("page").value;
+  
+    if (title.length > 0) {
+      add_url += "judul=" + title;
+    }
+    if (sort.length > 0) {
+      add_url += "&sort=" + sort;
+    }
+    if (filter.length > 0) {
+      add_url += "&genre=" + filter;
+    }
+    if (page.length > 0) {
+      add_url += "&page=" + page;
+    }
+  } catch (e) {
+    console.log(e);
   }
-  if (sort.length > 0) {
-    base_url += "&sort=" + sort;
-  }
-  if (filter.length > 0) {
-    base_url += "&genre=" + filter;
-  }
-  if (page.length > 0) {
-    base_url += "&page=" + page;
-  }
+  
 
   const xhr = new XMLHttpRequest();
 
+  if(data != ""){
+    final_url = base_url + data;
+    add_url = data;
+  } else {
+    final_url = base_url + add_url;
+  }
+  console.log(final_url)
 
-  xhr.open("GET", base_url, true);
+  xhr.open("GET", final_url, true);
   xhr.onload = function () {
     if (this.status == 200) {
       console.log(this.responseText);
       let response = JSON.parse(this.responseText);
       let songList = document.getElementById("song-list");
       songList.innerHTML = "";
+      window.history.pushState("","","/?search/" + add_url);
       response.forEach((song) => {
         songList.innerHTML += `
             <div class="song-item">
@@ -37,8 +75,28 @@ const searchSong = () => {
               </div>
             </div>
           `;
+        
       });
     }
   };
   xhr.send();
 };
+
+const prevPage = () => {
+  let newPage = parseInt(getQueryVariable("page")) - 1
+  if (newPage == 0){
+    newPage = 1
+  }
+
+  const link = updateQueryStringParameter(window.location.href, "page", newPage);
+  
+  searchSong((link.split("?")[1]).split("/")[1]);
+}
+
+const nextPage = () => {
+  let newPage = parseInt(getQueryVariable("page")) + 1
+
+  const link = updateQueryStringParameter(window.location.href, "page", newPage);
+  
+  searchSong((link.split("?")[1]).split("/")[1]);
+}
