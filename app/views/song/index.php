22b68da775703811f6bd74da421c6d5e263c6386
lyorafelicya <13520073@std.stei.itb.ac.id>
2022-10-27 23:58:04 +0700

    add hyperlink to song album

--- app/views/song/index.php
+++ app/views/song/index.php
@@ -53,8 +53,8 @@
 
                                 <div class='song-info'>
                                     <span class='song-title' id='song-title'>Loading title..</span>
+                                    <span class='song-album'><a href="/?album/detail/"></a></span>
                                     <span class='song-artist'>Loading artist..</span>
-                                    <span class='song-album'>Loading album..</span>
                                     <span class='release-date' id='song-release-date'>Loading..</span>
 
                                     <span class='duration'><i class="fa fa-clock" id='song-duration'></i> Loading..</span>
